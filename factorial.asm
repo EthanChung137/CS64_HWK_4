09af2d6ff5b87fe6d5d968e338522c4620c9f54e
Ethan Chung <ethanchung@cs.ucsb.edu>
2025-02-01 01:12:38 -0800

    Finished factorial.asm

--- factorial.asm
+++ factorial.asm
@@ -11,14 +11,49 @@
 
 #Data Area (i.e. memory setup directive)
 .data
-	string1	.asciiz	"Enter a number:\n"
+	InputString:	.asciiz	"Enter a number:\n"
 	# TODO: Write the rest of the initializations here
-
+	FactorialOf: 	.asciiz "Factorial of "
+	Is: 		.asciiz " is:\n"
+	Newline:	.asciiz "\n"
 #Text Area (i.e. instructions/code directive)
 .text
 main:
 
 	# TODO: Write your code here
+	input:
+	la $a0, InputString
+	li $v0, 4
+	syscall
+	li $v0, 5
+	syscall
+	li $t0, 2
+	move $t1, $v0
+	move $t2, $v0
+	loop:
+	beq $t0, $v0, print
+	mult $t1, $t0
+	mflo $t1
+	addi $t0, 1
+	j loop
+	print:
+	la $a0, FactorialOf
+	li $v0, 4
+	syscall
+	move $a0, $t2
+	li $v0, 1
+	syscall
+	la $a0, Is
+	li $v0, 4
+	syscall 
+	move $a0, $t1
+	li $v0, 1
+	syscall
+	la $a0, Newline
+	li $v0, 4
+	syscall
 
 exit:
 	# TODO: Write code to properly exit a SPIM simulation
+	li $v0, 10
+	syscall
